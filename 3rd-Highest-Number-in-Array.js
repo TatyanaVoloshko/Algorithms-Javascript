1685dc653b3a50264a5b457bcd35eb37cc6b5c93
TatyanaVoloshko <106927709+TatyanaVoloshko@users.noreply.github.com>
2024-02-09 13:08:49 +0100

    added algorithm: 3rd Highest number in arr

---
+++ 3rd-Highest-Number-in-Array.js
@@ -0,0 +1,31 @@
+const arr = [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31];
+
+let first = 0
+let second = 0
+let third = 0
+
+for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > first) {
+        third = second
+        second = first
+        first = arr[i]
+    } else if (arr[i] > second) {
+        third = second
+        second = arr[i]
+    } else if (arr[i] > third) {
+        third = arr[i]
+    }
+}
+
+console.log(third)
+
+
+// define an array with numbers
+// define three variables with a starting value of 0
+// for loop to interate through array
+// first condition to check if the array element is bigger than first
+// if true shift first value to second, second value to third and assayn first the current element value
+// if false second condition check if array element is bigger then second
+// if true shift second value to the three and assin second the current element value
+// if false third condition / if set true third to the current element 
+// print third
