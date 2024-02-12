203deb30896d971bdb657f9e06ca6b42df395b52
TatyanaVoloshko <106927709+TatyanaVoloshko@users.noreply.github.com>
2024-02-12 21:27:04 +0100

    added script find the longest word in a string

---
+++ script-Find-the-longest-Word-in-a-String.js
@@ -0,0 +1,20 @@
+// Tuesday: Find the longest Word in a String
+// Return the length of the longest word in the provided sentence.
+
+// Your response should be a number. For these algorithms, you are allowed to use the split() method.
+
+const findLongestWord = (string) => {
+  let arrOfString = string.split(" ");
+  let longWord = 0;
+
+  for (let i = 0; i < arrOfString.length; i++) {
+    if (arrOfString[i].length > longWord) {
+      longWord = arrOfString[i].length;
+    }
+  }
+  console.log(longWord);
+};
+
+findLongestWord("The quick brown fox jumped over the lazy dog"); // should return a number.
+findLongestWord("The quick brown fox jumped over the lazy dog"); // should return 6.
+findLongestWord("May the force be with you"); // should return 5.
