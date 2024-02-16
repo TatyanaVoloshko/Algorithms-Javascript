7c950c45b168a23e2288b70fecdf383c482d8a17
TatyanaVoloshko <106927709+TatyanaVoloshko@users.noreply.github.com>
2024-02-16 10:40:36 +0100

    added bubble sort script

---
+++ script-Bubble-Sort.js
@@ -0,0 +1,24 @@
+// define function with parametr arr
+// for loop arr start from last element
+// for loop arr start from 1 to last element
+// condition arr[j-1] > arr[j]
+//  define new variable temp equal arr[j-1]
+// if true arr[j-1] = arr[j], arr[j] = temp
+// return arr
+
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = 1; j <= i; j++) {
+      if (arr[j - 1] > arr[j]) {
+        let temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([7, 5, 2, 4, 3, 9]));
+console.log(bubbleSort([9, 7, 5, 4, 3, 1]));
+console.log(bubbleSort([1, 2, 3, 4, 5, 6]));
--- script-Fibonacci-Sequence.js
+++ script-Fibonacci-Sequence.js
@@ -5,20 +5,38 @@
 // change a = b, b = c
 // rturn b
 
-function fib(n) {
-    let a = 0;
-    let b = 1;
+// function fib(n) {
+//     let a = 0;
+//     let b = 1;
 
-    for (let i = 2; i <= n; i += 1) {
-        let c = a + b
-        a = b
-        b = c
-    }
-    return b;
-}
+//     for (let i = 2; i <= n; i += 1) {
+//         let c = a + b
+//         a = b
+//         b = c
+//     }
+//     return b;
+// }
 
-console.log(fib(43))
-console.log(fib(101));
-console.log(fib(227));
-console.log(fib(491));
-console.log(fib(881));
+// console.log(fib(43))
+// console.log(fib(101));
+// console.log(fib(227));
+// console.log(fib(491));
+// console.log(fib(881));
+
+// let x = [5, 1, 9, 4, 3, 7]
+
+// function sort(arr) {
+//     let newArr = []
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[0] > arr[i]) {
+//             arr[0] = arr[i]
+           
+//         }
+//          return newArr.push(arr[i]);
+//     }
+    
+// }
+
+// console.log(sort(x))
+
